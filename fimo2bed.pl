3be10c3b1f6629b32a822b1b41207fcf5a1dc00c
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-08-25 09:48:58 -0400

    convert fimo output to bed format

--- fimo2bed.pl
+++ fimo2bed.pl
@@ -7,7 +7,7 @@ Program to convert fimo.txt to bed file
 fimo2bed.pl fimo.txt
 );
 
-my $CUTOFF = 0.05;
+my $CUTOFF = 1e-5;
 # requirement check
 if (scalar(@ARGV) != 1) {
     print $usage;
@@ -19,7 +19,7 @@ open FIMO, $ARGV[0] or die print $!;
 my $name = ($ARGV[1])?$ARGV[1]:$ARGV[0];  
 $name =~ s/.txt//;
 
-open(my $bed, '>', 'fimo001.bed');
+open(my $bed, '>', 'allgenes.bed');
 
 print $bed "track name=$name description=$name useScore=1 \n";
 
@@ -27,12 +27,9 @@ print $bed "track name=$name description=$name useScore=1 \n";
 while(<FIMO>){
 	chomp; 
 	my @r = split('\t');
-	#print $r[6],$r[7];
-	#print "\n";
-	next if $r[7] > $CUTOFF;
-	$r[7]=-log($r[7])*100;
-	$r[7] = 1000 if $r[7] > 1000;
-	print $bed join("\t",@r[1,2,3,0,7,4]),"\n";
+	# Use p value
+	next if $r[6] > $CUTOFF;
+	$r[6]=-log($r[6])*50;
+	$r[6] = 1000 if $r[6] > 1000;
+	print $bed join("\t",@r[1,2,3,0,6,4]),"\n";
 }
-
-close $bed;
